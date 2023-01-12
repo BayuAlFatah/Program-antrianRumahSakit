7b1c766370354fbd1cf4f732ac6b64e5706a9521
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-12 15:34:20 +0700

    FIX BUG DAN PROGRAM BERJALAN DENGAN BENAR

--- antrianRS.java
+++ antrianRS.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 
 public class antrianRS {
     public static void main(String[] args) {
-       main();
-        
-    }
-    private static void main(){
-        boolean ulang = true;
+        boolean ulang = true; // UNTUK PER-ULANGAN WHILE
+        int urutan = 001; // UNTUK ANGKA ANTRIAN KELAS VVIP
+        int b = 001; //UNTUK ANGKA ANTRIAN KELAS BPJS
         while (ulang) {
             Scanner inputUser = new Scanner(System.in);
             System.out.println("selamat datang di rumah sakit bayu");
@@ -16,50 +14,65 @@ public class antrianRS {
             System.out.println(" 2. untuk BPJS");
             System.out.println(" 3. untuk keluar");
             System.out.println("pencet angka untuk memilih : ");
-            var input = inputUser.nextInt();
+            var input = inputUser.nextInt(); // UNTUK MENERIMA INPUT YANG BERSIFAT INT
            if (input == 1) {
-            vvip();
+            System.out.println("==== RSUD PASAR MINGGU ====");
+            System.out.println("|| \tkelas VVIP   \t ||");
+            System.out.println("||   NOMOR ANTRIAN ANDA  ||");
+            System.out.println("-------------------");
+            System.out.println("|   A " +  urutan + "   |");
+            System.out.println("-------------------");
+            urutan++; //INCREMENT
            }else if(input == 2){
-            bpjs();
-           }else if(input == 3){
+            System.out.println("==== RSUD PASAR MINGGU ====");
+            System.out.println("|| \tkelas BPJS   \t ||");
+            System.out.println("||   NOMOR ANTRIAN ANDA  ||");
+            System.out.println("-------------------");
+            System.out.println("|   B " +  b + "   |");
+            System.out.println("-------------------");
+            b++; //INCREMENT
+           }else if(input == 3){ //JIKA MEMILILH 3 MAKA AKAN KELUAR DARI PROGRAM
             break;
-           }else{
+           }else{ //JIKA PILIHAN TIDAK DITEMUKAN
             System.out.println("pilihan tidak ditemukan silahkan memilih dengan benar");
            }
         }
     }
+    private static void main(){
+       
+    }
     
-    protected static void vvip(){
-        boolean input = true;
+    // protected static void vvip(){
+    //     boolean input = true;
         
-        while(input){
-            int urutan = 1;
-            System.out.println("RSUD PASAR MINGGU");
-            System.out.println("kelas VVIP ");
-            System.out.println("NOMOR ANTRIAN ANDA");
-            System.out.println("-------------------");
-            System.out.println("|   A " +  urutan + "   |");
-            System.out.println("-------------------");
-            urutan++;
-            break;
-        }
+    //     while(input){
+    //         int urutan = 1;
+    //         System.out.println("RSUD PASAR MINGGU");
+    //         System.out.println("kelas VVIP ");
+    //         System.out.println("NOMOR ANTRIAN ANDA");
+    //         System.out.println("-------------------");
+    //         System.out.println("|   A " +  urutan + "   |");
+    //         System.out.println("-------------------");
+    //         urutan++;
+    //         break;
+    //     }
         
        
-    }
-    protected static void bpjs(){
+    // }
+    // protected static void bpjs(){
        
-        boolean input = true;
-        while (input) {
-            int b = 1;  
-            System.out.println("RSUD PASAR MINGGU");
-            System.out.println("kelas BPJS ");
-            System.out.println("NOMOR ANTRIAN ANDA");
-            System.out.println("-------------------");
-            System.out.println("|   B " +  b + "   |");
-            System.out.println("-------------------");
-            b++;
-            main();
-        }
+    //     boolean input = true;
+    //     while (input) {
+    //         int b = 1;  
+    //         System.out.println("RSUD PASAR MINGGU");
+    //         System.out.println("kelas BPJS ");
+    //         System.out.println("NOMOR ANTRIAN ANDA");
+    //         System.out.println("-------------------");
+    //         System.out.println("|   B " +  b + "   |");
+    //         System.out.println("-------------------");
+    //         b++;
+    //         main();
+    //     }
        
-    }
+    // }
 }
